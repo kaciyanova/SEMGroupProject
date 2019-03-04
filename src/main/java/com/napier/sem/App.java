9022cb00e35610120ecc5efb27088a292be9c383
Elliot\Kaci <kaciyanova@gmail.com>
2019-03-04 19:22:01 +0000

    Added Country & Language classes, set up methods for pulling city,country, and language lists from db into lists

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,6 +1,8 @@
 package com.napier.sem;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App
 {
@@ -10,14 +12,14 @@ public class App
         App a = new App();
 
         // Connect to database
-        a.connect();
+        a.Connect();
         // Get City
-        City city = a.getCity(1);
+//        City city = a.GetCity(1);
         // Display results
-        a.displayCity(city);
+//        a.DisplayCity(city);
 
         // Disconnect from database
-        a.disconnect();
+        a.Disconnect();
     }
 
     /**
@@ -28,39 +30,30 @@ public class App
     /**
      * Connect to the MySQL database.
      */
-    public void connect()
+    public void Connect()
     {
-        try
-        {
+        try {
             // Load Database driver
             Class.forName("com.mysql.jdbc.Driver");
-        }
-        catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
         }
 
         int retries = 10;
-        for (int i = 0; i < retries; ++i)
-        {
+        for (int i = 0; i < retries; ++i) {
             System.out.println("Connecting to database...");
-            try
-            {
+            try {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
-            }
-            catch (SQLException sqle)
-            {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+            } catch (SQLException sqle) {
+                System.out.println("Failed to Connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
-            }
-            catch (InterruptedException ie)
-            {
+            } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
@@ -69,69 +62,142 @@ public class App
     /**
      * Disconnect from the MySQL database.
      */
-    public void disconnect()
+    public void Disconnect()
     {
-        if (con != null)
-        {
-            try
-            {
+        if (con != null) {
+            try {
                 // Close connection
                 con.close();
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
         }
     }
-    public City getCity(int cityID)
+
+    public void GetWorldInfo()
     {
-        try
-        {
+        //pulling all from db since amount of data is small
+    }
+
+    //gets list of all countries in db
+    public List<Country> GetCountries()
+    {
+        try {
             // Create an SQL statement
-            Statement stmt = con.createStatement();
+            Statement query = con.createStatement();
             // Create string for SQL statement
-            String strSelect =
-                    "SELECT ID, Name, CountryCode "
-                            + "FROM city "
-                            + "WHERE ID = " + cityID;
+            String selectString =
+                    "SELECT Code, Name, Continent, Region,Population,Capital "
+                            + "FROM country "
+                            + "ORDER BY country.Code ASC";
+            ;
+
             // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
+            ResultSet results = query.executeQuery(selectString);
             // Return new city if valid.
             // Check one is returned
-            if (rset.next())
-            {
-                City city = new City();
-                city.ID = rset.getInt("ID");
-               // city.Name = rset.getString("Name");
-               // city.CountryCode = rset.getString("CountryCode");
-                //city.District = rset.getString("District");
-               // city.Population = rset.getInt("Population");
+            ArrayList<Country> countries = new ArrayList<Country>();
 
-                return city;
+            while (results.next()) {
+                Country country = new Country();
+                country.Code = results.getString("Code");
+                country.Name = results.getString("Name");
+                country.Continent = results.getString("Continent");
+                country.Region = results.getString("Region");
+                country.Population = results.getInt("Population");
+                country.Capital=results.getInt("Capital");
+
+                countries.add(country);
             }
-            else
-                return null;
-        }
-        catch (Exception e)
-        {
+            return countries;
+        } catch (Exception e) {
             System.out.println(e.getMessage());
-            System.out.println("Failed to get City details");
+            System.out.println("Failed to get Countries");
             return null;
         }
     }
 
-    public void displayCity(City city)
+    public ArrayList<City> GetCities()
     {
-        if (city != null)
-        {
+        try {
+            // Create an SQL statement
+            Statement query = con.createStatement();
+            // Create string for SQL statement
+            String selectString =
+                    "SELECT ID, CountryCode, Name, Continent, District,Population "
+                            + "FROM country "
+                            + "ORDER BY country.Code ASC";
+            ;
+
+            // Execute SQL statement
+            ResultSet results = query.executeQuery(selectString);
+            // Return new city if valid.
+            // Check one is returned
+            ArrayList<City> cities = new ArrayList<City>();
+
+            while (results.next()) {
+                City city = new City();
+                city.ID = results.getInt("Code");
+                city.Name = results.getString("Name");
+                city.CountryCode = results.getString("CountryCode");
+                city.District = results.getString("District");
+                city.Population = results.getInt("Population");
+
+                cities.add(city);
+            }
+            return cities;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get Cities");
+            return null;
+        }
+    }
+
+    public ArrayList<Language> GetLanguages()
+    {
+        try {
+            // Create an SQL statement
+            Statement query = con.createStatement();
+            // Create string for SQL statement
+            String selectString =
+                    "SELECT CountryCode, Name, Percentage "
+                            + "FROM country "
+                            + "ORDER BY country.Code ASC";
+            ;
+
+            // Execute SQL statement
+            ResultSet results = query.executeQuery(selectString);
+            // Return new city if valid.
+            // Check one is returned
+            ArrayList<Language> languages = new ArrayList<Language>();
+
+            while (results.next()) {
+                Language language = new Language();
+                language.Name = results.getString("Name");
+                language.CountryCode = results.getString("CountryCode");
+                language.Percentage = results.getFloat("Population");
+
+                languages.add(language);
+            }
+            return languages;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get languages");
+            return null;
+        }
+    }
+
+
+    public void DisplayCity(City city)
+    {
+        if (city != null) {
             System.out.println(
                     city.ID + " "
-                          //  + city.Name + " "
-                          //  + "Country code:" + city.CountryCode + "\n"
-                          //  + "District:" + city.District + "\n"
-                           // + "Population:" +city.Population + "\n"
-                    );
+                    //  + city.Name + " "
+                    //  + "Country code:" + city.CountryCode + "\n"
+                    //  + "District:" + city.District + "\n"
+                    // + "Population:" +city.Population + "\n"
+            );
         }
     }
 }
---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,31 @@
+package com.napier.sem;
+
+public class Country {
+    /**
+     * Country code
+     */
+    public String Code;
+
+    /**
+     * Country name
+     */
+    public String Name;
+
+    /**
+     * Country's  Code
+     */
+    public String Continent;
+
+    /**
+     * Country's Region
+     */
+    public String Region;
+
+    /**
+     * Country's Population
+     */
+    public int Population;
+
+    //Capital city ID
+    public int Capital;
+}
