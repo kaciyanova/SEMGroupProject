db16b3bc2017c6cabdf66b63d0bcdac55df0b41c
Thomas Meldrum <40319883@live.napier.ac.uk>
2019-03-06 11:55:02 +0000

    Adding new branch and Capital City report data.

---
+++ src/main/java/com/napier/sem/CapitalCity.java
@@ -0,0 +1,19 @@
+package com.napier.sem;
+public class CapitalCity {
+
+    /**
+     * Capital City name
+     */
+    public String Name;
+    /**
+     * Country's Population
+     */
+    public int Population;
+
+    /**
+     * Country Name
+     */
+    public String Country;
+
+
+}
