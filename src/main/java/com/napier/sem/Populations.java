bbdf71df5d53e8a42c0fb94e8f5d3fcfdd075ce4
leone kelly <40317391@live.napier.ac.uk>
2019-05-10 18:42:02 +0100

    test to population numbers unit test(not done)

---
+++ src/main/java/com/napier/sem/Populations.java
@@ -0,0 +1,23 @@
+package com.napier.sem;
+
+public class Populations {
+
+    //Country name
+    public String Name;
+
+    //Country's region
+
+    public String Region;
+
+    public String continent;
+
+    //Country's Population (In cities and rural %)
+
+    public String Capital;
+    public String Rural;
+    public int CityPopulation;
+    public float PopulationPercentage;
+
+
+    //Needs more work done (In progress)
+}
