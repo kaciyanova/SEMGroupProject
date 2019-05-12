ee67f165794c9b547656aa28b8adf1fdf013e8de
thomas <ezfragstoday@gmail.com>
2019-05-12 01:32:07 +0100

    Adding unit tests - In progress - Nearly Complete - Reverting

---
+++ src/test/java/com/napier/sem/PopulationPercentagesTests.java
@@ -0,0 +1,111 @@
+package com.napier.sem;
+
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static com.napier.sem.App.*;
+import static com.napier.sem.PopulationPercentages.*;
+
+//(WIP)
+
+public class PopulationPercentagesTests {
+
+    @BeforeAll
+    static void init() {
+
+    }
+
+//     @Test
+//     void GetContinentPopulationStatsTestNull()
+//     {
+//         GetContinentPopulationStats(null);
+//     }
+
+//     @Test
+//    void GetContinentPopulationTestEmpty()
+//    {
+//        ArrayList<Country> countriesInArea = new ArrayList<>();
+//        GeneratePopulationReport(countriesInArea);
+//    }
+
+//    @Test
+//    void GetContinentPopulationTestNormal()
+//    {
+//       ReportGenerator.GeneratePopulationReport(CalculateAreaPopulations(countriesInArea,continentPopulationStats));
+//    }
+//
+
+//     @Test
+//     void GetRegionPopulationStatsTestNull()
+//     {
+//         GetRegionPopulationStats(null);
+//     }
+
+//     @Test
+//    void GetRegionPopulationStatsTestEmpty()
+//    {
+//        ArrayList<Country> countriesInArea = new ArrayList<>();
+//        GeneratePopulationReport(countriesInArea);
+//    }
+
+//     @Test
+//    void GetRegionPopulationStatsTestNormal()
+//    {
+//       ReportGenerator.GeneratePopulationReport(CalculateAreaPopulations(countriesInArea,regionPopulationStats));
+//    }
+
+
+//     @Test
+//     void GetCountryPopulationStatsTestNull()
+//     {
+//         GetCountryPopulationStats(null);
+//     }
+
+//     @Test
+//    void GetCountryPopulationStatsTestEmpty()
+//    {
+//        ArrayList<Country> requestedCountry  = new ArrayList<>();
+//        GeneratePopulationReport(requestedCountry);
+//    }
+
+//     @Test
+//    void GetCountryPopulationStatsTestNormal()
+//    {
+//        ReportGenerator.GeneratePopulationReport(populationStatistics);
+//    }
+
+
+//     @Test
+//     void CalculateAreaPopulationsTestNull()
+//     {
+//         CalculateAreaPopulations(null);
+//     }
+
+//     @Test
+//     void CalculateAreaPopulationsTestNormal()
+//     {
+//         ArrayList<Country> country  = new ArrayList<>();
+//        countryPopulationStatistics(country);
+//         PopulationStatistics countryPopulationStatistics = CalculateCountryPopulationStats(country);
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
