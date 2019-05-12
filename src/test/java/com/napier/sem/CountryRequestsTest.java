2fa85c85e8b93ab5902543d20bf9b6d2378a7af7
leone kelly <40317391@live.napier.ac.uk>
2019-05-12 18:13:33 +0100

    test to country reequest

---
+++ src/test/java/com/napier/sem/CountryRequestsTest.java
@@ -0,0 +1,57 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class CountryRequestsTest {
+}
+    @BeforeAll
+    public static void init()
+    {
+
+    }
+
+    @Test
+    void GetCountriesInAreaByPopulationTestNull()
+    {
+        GetCountriesInAreaByPopulation(null, null);
+    }
+
+    private void GetCountriesInAreaByPopulation(Object o, Object o1) {
+    }
+
+    @Test
+    void getWorldPopulationTestNull()
+    {
+        getWorldPopulation(null, null);
+    }
+
+    private void getWorldPopulation(Object o, Object o1) {
+    }
+
+    @Test
+    void getContinentPopulationTestNull()
+    {
+        getContinentPopulation(null, null);
+    }
+
+    private void getContinentPopulation(Object o, Object o1) {
+    }
+
+    @Test
+    void ggetRegionPopulationTestNull()
+    {
+        getRegionPopulation(null, null);
+    }
+
+    private void getRegionPopulation(Object o, Object o1) {
+    }
+
+    @Test
+    void ggetRegionPopulationTestNull()
+    {
+        getRegionPopulation(null, null);
+    }
+
+    private void getRegionPopulation(Object o, Object o1) {
+    }
